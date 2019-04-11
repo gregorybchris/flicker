144328a61f4134d7c4865d23558db3f4b2114867
gregorybchris <gregorybchris@gmail.com>
2019-04-10 23:18:02 -0700

    Add flicker main

---
+++ flicker.py
@@ -0,0 +1,20 @@
+import requests 
+  
+
+def get_mortality_data():
+    api_endpoint = 'https://mort-vis.herokuapp.com/summary'
+    
+    params = {
+        'age': 23,
+        'race': 'White',
+        'gender': 'M'
+    }
+
+    response = requests.get(url=api_endpoint, params=params)
+    return response.json()
+
+if __name__ == '__main__':
+    print("Flicker is running")
+    data = get_mortality_data()
+    deaths = data['deaths']
+    print(f"Got deaths: {deaths}")
