6abfb5dd2929190ccc7533a6d8d359290e6e12ea
gregorybchris <gregorybchris@gmail.com>
2019-04-12 02:02:33 -0700

    Fix client_api

--- client_api.py
+++ client_api.py
@@ -6,7 +6,7 @@ class ClientAPI:
     DEFAULT_URL = "https://flicker.appspot.com"
 
     def __init__(self, api_url=None):
-        self.api_url = api_url if api_url is not None else DEFAULT_URL
+        self.api_url = api_url if api_url is not None else ClientAPI.DEFAULT_URL
 
     def post_message(self, message):
         endpoint = os.path.join(API_URL, 'message')
