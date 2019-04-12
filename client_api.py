d585c67479bc616896ec811e94a552fdaf2fba91
gregorybchris <gregorybchris@gmail.com>
2019-04-12 02:04:13 -0700

    Use default API URL

--- client_api.py
+++ client_api.py
@@ -6,15 +6,15 @@ class ClientAPI:
     DEFAULT_URL = "https://flicker.appspot.com"
 
     def __init__(self, api_url=None):
-        self.api_url = api_url if api_url is not None else ClientAPI.DEFAULT_URL
+        self._api_url = api_url if api_url is not None else ClientAPI.DEFAULT_URL
 
     def post_message(self, message):
-        endpoint = os.path.join(API_URL, 'message')
+        endpoint = os.path.join(self._api_url, 'message')
         body = {'message': message}
         r = requests.post(url=endpoint, data=body)
 
     def get_messages(self):
-        endpoint = os.path.join(API_URL, 'messages')
+        endpoint = os.path.join(self._api_url, 'messages')
 
         response = requests.get(url=endpoint)
         return response.json()
