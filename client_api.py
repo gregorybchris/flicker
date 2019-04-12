73188847db297212a44eda3d2db3249a7e88e34e
gregorybchris <gregorybchris@gmail.com>
2019-04-12 02:34:48 -0700

    Fixed post headers

--- client_api.py
+++ client_api.py
@@ -1,20 +1,27 @@
+import json
 import os
 import requests
 
 
 class ClientAPI:
     DEFAULT_URL = "https://flicker.appspot.com"
+    DEFAULT_HEADERS = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 
-    def __init__(self, api_url=None):
+    def __init__(self, api_url=None, extra_headers=None):
         self._api_url = api_url if api_url is not None else ClientAPI.DEFAULT_URL
+        self._headers = ClientAPI.DEFAULT_HEADERS
+        if extra_headers is not None:
+            for k, v in extra_headers.items():
+                self._headers[k] = v
 
     def post_message(self, message):
         endpoint = os.path.join(self._api_url, 'message')
-        body = {'message': message}
-        r = requests.post(url=endpoint, data=body)
+        body = json.dumps({'message': message})
+        return requests.post(url=endpoint, headers=self._headers, data=body)
 
     def get_messages(self):
         endpoint = os.path.join(self._api_url, 'messages')
-
-        response = requests.get(url=endpoint)
-        return response.json()
+        return requests.get(url=endpoint)
--- flicker.py
+++ flicker.py
@@ -1,8 +1,17 @@
 import client_api
+import pprint
 
 
 if __name__ == '__main__':
     print("Flicker is running")
     api = client_api.ClientAPI()
-    api.post_message("Flicker!")
-    print("Posted a new message to Flicker!")
+
+    message = "Flicker!"
+    print("Posting message: ", message)
+    response = api.post_message(message)
+    print("Received: ", response.status_code, response.json())
+
+    response = api.get_messages()
+    messages = response.json()['messages']
+    print("Get messages:")
+    pprint.pprint(messages)
