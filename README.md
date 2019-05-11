5b482b9de44610f81384eb37ad8e2509e030968b
gregorybchris <gregorybchris@gmail.com>
2019-05-10 21:56:54 -0700

    Move camera out of probe

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Flicker Microcomputer
 
 This is the microcomputer code for Flicker
+
+## Dependencies
+
+```bash
+sudo apt-get install libjpeg-dev
+pip install -r requirements.txt
+```
--- camera_sensor.py
+++ camera_sensor.py
@@ -9,12 +9,12 @@ from PIL import Image, ImageStat
 class CameraSensor(sensor_base.Sensor):
     def setup(self, temp_dir='/tmp'):
         self._temp_dir = temp_dir
+        self._camera = picamera.PiCamera()
 
     def probe(self, full_image=False):
-        camera = picamera.PiCamera()
         filename = 'photo-{}.jpg'.format(uuid.uuid4())
         filepath = os.path.join(self._temp_dir, filename)
-        camera.capture(filepath)
+        self._camera.capture(filepath)
         image = Image.open(filepath)
         if full_image:
             return image
