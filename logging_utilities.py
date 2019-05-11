614096596f4484b73b64e84e6deaa8a0cefd8699
gregorybchris <gregorybchris@gmail.com>
2019-05-11 00:19:13 -0700

    Add logging formatter

--- logging_utilities.py
+++ logging_utilities.py
@@ -4,6 +4,9 @@ import logging
 def get_logger(filename='flicker_debug.log'):
     logger = logging.getLogger(__name__)
     handler = logging.FileHandler(filename)
+    format_str = '%(asctime)s - %(levelname)-8s - %(message)s'
+    formatter = logging.Formatter(format_str)  
+    handler.setFormatter(formatter)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     return logger
